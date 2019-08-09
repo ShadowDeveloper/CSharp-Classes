a3f7bb5f84d87cff954c164a623c635c5e99c4ea
ShadowDeveloper <ro_web1@hotmail.com>
2019-08-09 07:19:58 -0300

    Subindo projeto

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 2 - Executando Variaveis");
+
+            int idade = 26;
+            Console.WriteLine("idade: " + idade);
+
+            idade = 10 + 5;
+            Console.WriteLine("idade somado: " + idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("idade multiplicando: " + idade);
+
+            Console.WriteLine("Execução finalizada, tecle enter para encerrar");
+            Console.ReadLine();
+        }
+    }
+}
