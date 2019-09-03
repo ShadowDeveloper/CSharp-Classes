9ad9b9438e4b547e346bd3c5aef3153d4c9e7c16
Rodrigo Souza <rodrigo.souza@norber.com.br>
2019-09-03 13:11:20 -0300

    Namespace composição e null

---
+++ csharp-formation/2 - Introduction-to-object-orientation/ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente grabriela = new Cliente
+            {
+                nome = "Gabriela",
+                cpf = "39906525835",
+                profissao = "Gerente de supermercados"
+            };
+
+            ContaCorrente conta = new ContaCorrente
+            {
+                titular = grabriela,
+                agencia = 563,
+                numero = 5634524,
+            };
+
+            conta.titular.nome = "Gabriela Costa";
+
+            Console.WriteLine(grabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            //Console.ReadKey();
+        }
+    }
+}
