a03cea44bb0535e4fcc7e415141ff3044f646f55
Rodrigo Souza <rodrigo.souza@norber.com.br>
2019-10-04 17:36:50 -0300

    Iniciando com métodos estáticos

---
+++ csharp-formation/2 - Introduction-to-object-orientation/ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(8555,544788947);
+            Console.WriteLine("Ag: " + conta.Agencia);
+            Console.WriteLine("Num Conta: " + conta.Numero);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            ContaCorrente contaDoRodrigo = new ContaCorrente(867, 4554477);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+            Console.ReadLine();
+        }
+    }
+}
