18ac17d06471a08e3e2d66c9e6af220c207a9ab9
Rodrigo Souza <rodrigo.souza@norber.com.br>
2019-11-13 19:41:10 -0300

    Adicionando classe funcionario

---
+++ csharp-formation/3 - understanding-inheritance-interface/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+    }
+}
