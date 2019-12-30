58a166bc2367a6e1404e75cbb2e766552a3d21cd
Rodrigo Souza <rodrigo.souza@norber.com.br>
2019-12-30 17:56:39 -0300

    Iniciando com Herança

---
+++ csharp-formation/3 - understanding-inheritance-interface/ByteBank/ByteBank_2/Funcionarios/Diretor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_2.Funcionarios
+{
+    class Diretor : Funcionario // Isso se chama herança : Funcionario. O diretor também tem Nome, CPF e Salario
+    {
+        
+        public override double GetBonificacao()
+        {
+            return Salario;
+        }
+    }
+}
---
+++ csharp-formation/3 - understanding-inheritance-interface/ByteBank/ByteBank_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank_2.Funcionarios;
+
+
+namespace ByteBank_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario funcionario = new Funcionario()
+            {
+                Nome = "Rodrgo",
+                CPF = "39965554452",
+                Salario = 2000
+            };
+
+            Console.WriteLine(funcionario.Nome);
+            Console.WriteLine(funcionario.GetBonificacao());
+
+            gerenciador.Registrar(funcionario);
+
+            Console.WriteLine("=========================");
+
+            Diretor diretor = new Diretor()
+            {
+                Nome = "Ramirez",
+                CPF = "29965554452",
+                Salario = 5000,
+            };
+
+            Console.WriteLine(diretor.Nome);
+            Console.WriteLine("Bonificacao diretor: " + diretor.GetBonificacao());
+            gerenciador.Registrar(diretor);
+
+
+            Console.WriteLine("Total da bonificacao funcionários " + "R$" + gerenciador.GetTotalBonificacao());
+
+
+            Funcionario diretorTeste = diretor;
+
+            Console.WriteLine("Bonificação de uma referência de Diretor: " + diretor.GetBonificacao());
+            Console.WriteLine("Bonificação de uma referência de Funcionario: " + diretorTeste.GetBonificacao());
+
+            Console.ReadKey();
+
+        }
+    }
+}
