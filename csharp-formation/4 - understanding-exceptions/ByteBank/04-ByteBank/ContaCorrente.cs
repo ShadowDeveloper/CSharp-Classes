e0715e9f4202436b37ad5474b8c432b11fdb0829
Rodrigo Souza <rodrigo.souza@norber.com.br>
2020-01-08 17:22:34 -0300

    Iniciando com Lançando nossa exceção de argumento

---
+++ csharp-formation/4 - understanding-exceptions/ByteBank/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    public class ContaCorrente
+    {
+        private Cliente _titular;
+        private int _agencia;
+        private double _saldo = 100.45; //Underline para quando campo é privado e local
+        public Cliente Titular { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public static double TaxaOperacao { get; private set; }
+
+        public int Numero { get; }
+        public int Agencia { get; }
+
+        //Constructor
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (agencia <= 0 || numero <= 0)
+            {
+                Console.WriteLine(Titular.Nome);
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+            TotalDeContasCriadas++;
+        }
+        //Constructor
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _saldo = value;
+                }
+                return;
+            }
+        }
+
+
+        public void SetSaldo(double novoSaldo)
+        {
+            if (novoSaldo >= 0)
+            {
+                _saldo = novoSaldo;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente ContaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            _saldo -= valor;
+            ContaDestino.Depositar(valor);
+            return true;
+        }
+
+    }
+}
