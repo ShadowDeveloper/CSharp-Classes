f630c68cf323d24863aea86e706d007adca80b20
Rodrigo Souza <rodrigo.souza@norber.com.br>
2020-01-22 10:15:01 -0300

    Ajustes de ambiente e globalizacao

---
+++ flutter-formation/persistencia-com-flutter/bytebank01/lib/helper/constants.dart
@@ -0,0 +1,11 @@
+class ComumConstants {
+  final String _appBarTitle = "NewPontoCloud";
+
+  String get appBarTitle {
+    return _appBarTitle;
+  } // Um get ou Set pode ser feito assim
+
+  final String _contacts = "Contacts";
+
+  String get contacts => _contacts; // Um get ou Set pode ser feito assim
+}
---
+++ flutter-formation/persistencia-com-flutter/bytebank01/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:bytebank01/style/app_theme.dart';
+import 'package:bytebank01/screens/dashboard.dart';
+
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget{
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: lightAppTheme(),
+      darkTheme: darkAppTheme(),
+      home: Dashboard(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
