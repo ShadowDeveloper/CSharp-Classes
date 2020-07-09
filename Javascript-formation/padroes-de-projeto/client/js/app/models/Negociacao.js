b6ec8d8298e79d04091f95f0aa430977f4b26420
Rodrigo Souza <ro_web1@hotmail.com>
2020-07-09 11:14:26 -0300

    iniciando padroes de projeto em js

---
+++ Javascript-formation/padroes-de-projeto/client/js/app/models/Negociacao.js
@@ -0,0 +1,29 @@
+class Negociacao {
+
+    get quantity() {
+        return this._quantidade;
+    }
+
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    };
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return this.data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this.valor;
+    }
+}
